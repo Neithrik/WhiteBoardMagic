4892b3ecadf9b242699510211103dc08acf40df3
Erik Grabljevec <erik.grabljevec@synthesia.io>
2023-10-12 11:40:56 +0200

    Connect Studio w/ Backend App.

--- studio/src/App.js
+++ studio/src/App.js
@@ -1,8 +1,34 @@
 import Dropzone from 'react-dropzone'
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const processFiles = async (acceptedFiles) => {
+
+    const queryParams = new URLSearchParams({
+      name: acceptedFiles[0].name,
+    });
+
+    const requestUrl = `http://localhost:5000?${queryParams.toString()}`
+
+    const res = await fetch(
+      requestUrl,
+      {
+        method: "GET",
+        headers: { 
+          "Content-type": "application/json",
+        },
+      }
+    );
+
+    const response = await res.text();
+
+    setResponse(response);
+  };
+
+  const [response, setResponse] = useState(undefined);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,12 +46,13 @@ function App() {
         </a>
       </header>
 
-      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      <Dropzone onDrop={processFiles}>
         {({getRootProps, getInputProps}) => (
           <section>
             <div {...getRootProps()}>
               <input {...getInputProps()} />
               <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>Retrieved response is {response}</p>
             </div>
           </section>
         )}
